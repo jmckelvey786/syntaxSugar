0bf06399b2acd8c87f5b1312a590f6417d59b4ac
jmckelvey786 <mckelveywarner@gmail.com>
2021-02-24 20:44:40 -0500

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? "is less than nine" : "greater than or equal to nine";
+            Console.WriteLine($"{answer} {response}");
+
+        }
+    }
+}
